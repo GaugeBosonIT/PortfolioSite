9291c2cd55a86c05e196967499577ceabf9d4517
Harry McCarney <hmccarney@gmail.com>
2011-10-19 18:50:41 +0200

    completed about page

--- portfolio/Controllers/HomeController.cs
+++ portfolio/Controllers/HomeController.cs
@@ -13,97 +13,21 @@ namespace portfolio.Controllers
 
         public ActionResult Index()
         {
+            
+            ViewBag.PageTitle = "Discover your customers with enaging social commerce apps.";
             return View();
         }
         public ActionResult About()
         {
+            ViewBag.PageTitle = "About";
             return View();
         }
 
-        //
-        // GET: /Home/Details/5
-
-        public ActionResult Details(int id)
+        public ActionResult Services()
         {
+            ViewBag.PageTitle = "Services";
             return View();
         }
-
-        //
-        // GET: /Home/Create
-
-        public ActionResult Create()
-        {
-            return View();
-        } 
-
-        //
-        // POST: /Home/Create
-
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-        
-        //
-        // GET: /Home/Edit/5
- 
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /Home/Edit/5
-
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
- 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
-        // GET: /Home/Delete/5
- 
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /Home/Delete/5
-
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
- 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+     
     }
 }
