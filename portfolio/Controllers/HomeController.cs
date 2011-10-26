cc1cd758b824b5af548f57cd970044ebfe941649
Harry McCarney <hmccarney@gmail.com>
2011-10-26 21:15:46 +0200

    added projects and contact form

--- portfolio/Controllers/HomeController.cs
+++ portfolio/Controllers/HomeController.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using portfolio.Models;
+using System.Configuration;
+using System.Net.Mail;
 
 namespace portfolio.Controllers
 {
     public class HomeController : Controller
     {
+        public string username = ConfigurationManager.AppSettings["username"].ToString();
+        public string password = ConfigurationManager.AppSettings["password"].ToString();
+        public string SmtpClient = ConfigurationManager.AppSettings["SmtpClient"].ToString();
+        public string FromAddress = ConfigurationManager.AppSettings["FromAddress"].ToString();
+        public string EmailSubject = ConfigurationManager.AppSettings["EmailSubject"].ToString();
+            
+        
+       
         //
         // GET: /Home/
 
@@ -26,6 +37,40 @@ namespace portfolio.Controllers
         public ActionResult Services()
         {
             ViewBag.PageTitle = "Services.";
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            ViewBag.PageTitle = "Projects.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.PageTitle = "Contact";
+            ViewBag.MessageStatus = "";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(Contact m)
+        {
+
+            MailMessage mail = new MailMessage();
+            SmtpClient SmtpServer = new SmtpClient(SmtpClient);
+            mail.From = new MailAddress(FromAddress);
+            mail.To.Add(m.Email);
+            mail.Subject = m.Email;
+            mail.Body = m.Message;
+
+            SmtpServer.Port = 587;
+            SmtpServer.Credentials = new System.Net.NetworkCredential(username, password);
+            SmtpServer.EnableSsl = true;
+
+            SmtpServer.Send(mail);
+            ViewBag.PageTitle = "Contact";
+            ViewBag.MessageStatus = "Thanks! We will be in touch.";
             return View();
         }
      
---
+++ portfolio/Models/Contact.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace portfolio.Models
+{
+    public class Contact
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        [ScaffoldColumn(false)]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter an email")]
+        [RegularExpression(@"^[a-z][a-z|0-9|]*([_][a-z|0-9]+)*([.][a-z|0-9]+([_][a-z|0-9]+)*)?@[a-z][a-z|0-9|]*\.([a-z][a-z|0-9]*(\.[a-z][a-z|0-9]*)?)$", ErrorMessage ="Please enter a valid email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter a subject")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Please enter a message")]
+        public string Message { get; set; }
+
+    }
+}
